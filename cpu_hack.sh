02b1b42326a0a3488d0474fab59dd15b0d806696
bitcoin-generator <52181846+bitcoin-generator@users.noreply.github.com>
2022-07-31 23:47:45 -0300

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-apt update
-apt install screen -y
+sudo apt update
+sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
